7dceca2a952c12f4d36ea45b63b12f9280cfc420
Your Name <you@example.com>
2026-02-12 07:55:57 +0000

    Chnage the title

--- crates/stack-cli.com/src/components/hero.rs
+++ crates/stack-cli.com/src/components/hero.rs
@@ -6,7 +6,7 @@ pub fn Hero() -> Element {
         section {
             h1 {
                 class: "text-6xl max-w-3xl mx-auto text-center pb-6 font-semibold capitalize",
-                "Production-ready infrastructure for self-hosted web applications"
+                "Simplify Self Hosted Application and Database deployments"
             }
             h2 {
                 class: "subtitle max-w-3xl mx-auto text-center text-lg",
